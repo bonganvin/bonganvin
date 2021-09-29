a4a2afcba6780f6e39c5f0c97697a17cbc0126a6
bonganvin <bonganvin@gmail.com>
2021-09-29 09:04:56 +0200

    Programs

---
+++ Programs/Test002/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Layout1Component } from './layout1/layout1.component';
+import { Layout2Component } from './layout2/layout2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Layout1Component,
+    Layout2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
